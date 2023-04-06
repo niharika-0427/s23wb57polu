60c4b47eaed572160c4ea6b4a455ca6badf377c3
Niharika Polu <123121406+niharika-0427@users.noreply.github.com>
2023-04-05 21:49:07 -0500

    modified a files and added images and files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var boardRouter = require('./routes/board');
 var selectorRouter = require('./routes/selector');
-var carRouter = require('./routes/car');
+var quotesRouter = require('./routes/quotes');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/board', boardRouter);
 app.use('/selector', selectorRouter);
-app.use('/car', carRouter);
+app.use('/quotes', quotesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
